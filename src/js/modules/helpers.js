208d81b4b51ed6e579786cbe6f0a2a3b6a788fdf
Dmitry Onoprienko <donoprienko@justcoded.co>
2017-03-31 16:47:01 +0300

    stars

--- src/js/modules/helpers.js
+++ src/js/modules/helpers.js
@@ -2,10 +2,30 @@
 //
 // You can remove or add your own function in this file.
 
-var HP = {
-  random: function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+var DE = {
+  dotsEffect: function() {
+    var $cnt  = $('#home-canvas'),
+        cells = $(createCells());
+
+    $cnt.html(cells);
+
+    $('body').removeClass('load');
+
+    function createCells() {
+      var width  = 10,
+          height = 10,
+          size   = width * height,
+          html   = '<div class="entry-cells">';
+
+      for (var i = 0; i < size; i++) {
+        html += '<div class="cell cell-' + i + '"></div>';
+      }
+
+      html += '</div>';
+
+      return html;
+    }
   }
 };
 
-export default HP;
+export default DE;
