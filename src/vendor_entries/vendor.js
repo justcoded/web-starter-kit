ddbe1d22f5c100ea71cc7f52700b7bb2f500d1d0
kpoddubnyi <[poddubny.kirill@gmail.com]>
2017-07-12 18:53:26 +0300

    Create the With-Bootstrap template

--- src/vendor_entries/vendor.js
+++
@@ -1,7 +0,0 @@
-// All imports in this file will be compiled into vendors.js file.
-//
-// Note: ES6 support for these imports is not supported in base build
-
-module.exports = [
-  './node_modules/jquery/dist/jquery.js'
-];
