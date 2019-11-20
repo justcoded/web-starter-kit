820416af78cfb6699dabf9f85de27f0828a1938d
dsymonov <dsymonov@justcoded.co>
2019-11-20 17:57:26 +0200

    Add comments to vendor.js

--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -1,5 +1,6 @@
 // All imports in this file will be compiled into vendors.js file.
-
+//
+// For import ES6+ with transpiling to ES5, put sources into 'es6' array
 module.exports = {
   es5: [
     './node_modules/jquery/dist/jquery.js',
