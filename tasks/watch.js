ee4ca434a40129d028473052043c74f1e646235b
dsymonov <dsymonov@justcoded.co>
2019-11-22 18:43:20 +0200

    Change turn of watch…

--- tasks/watch.js
+++ tasks/watch.js
@@ -8,7 +8,7 @@ const gulp = require('gulp');
 module.exports = function (options) {
 
   return () => {
-    gulp.watch(`js/**/*`, gulp.series(options.tasks.buildCustomJs, options.tasks.esLint));
+    gulp.watch(`js/**/*`, gulp.series(options.tasks.esLint, options.tasks.buildCustomJs));
 
     gulp.watch(`scss/**/*`, gulp.series(options.tasks.buildSass, options.tasks.buildSassCustom));
 
