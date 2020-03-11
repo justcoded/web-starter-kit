538516cb89e8d07c1f15ec11f70e927fa84e09ce
Kassaila <dmytro.winter@gmail.com>
2020-03-11 22:51:29 +0200

    node support ^10. delete duplicate in gitignore. watch all files in vendors

--- tasks/watch.js
+++ tasks/watch.js
@@ -8,15 +8,15 @@ const gulp = require('gulp');
 module.exports = function (options) {
 
   return () => {
-    gulp.watch(`./${options.src}/js/**/*`, gulp.series(options.tasks.lintJs, options.tasks.buildJs));
+    gulp.watch(`./${options.src}/js/**/*.js`, gulp.series(options.tasks.lintJs, options.tasks.buildJs));
 
-    gulp.watch(`./${options.src}/scss/**/*`, gulp.series(options.tasks.buildStyles, options.tasks.buildStylesCustom));
+    gulp.watch(`./${options.src}/scss/**/*.scss`, gulp.series(options.tasks.buildStyles, options.tasks.buildStylesCustom));
 
-    gulp.watch(`./${options.src}/html/**/*`, gulp.series(options.tasks.buildHtml, options.tasks.lintHtml));
+    gulp.watch(`./${options.src}/html/**/*.html`, gulp.series(options.tasks.buildHtml, options.tasks.lintHtml));
 
-    gulp.watch(`./${options.src}/vendor_entries/vendor.js`, gulp.series(options.tasks.buildJsVendors));
+    gulp.watch(`./${options.src}/vendor_entries/**/*.js`, gulp.series(options.tasks.buildJsVendors));
 
-    gulp.watch(`./${options.src}/vendor_entries/vendor.scss`, gulp.series(options.tasks.buildStylesVendors));
+    gulp.watch(`./${options.src}/vendor_entries/**/*.scss`, gulp.series(options.tasks.buildStylesVendors));
 
     gulp.watch(options.filesToCopy)
       .on('unlink', (path) => {
